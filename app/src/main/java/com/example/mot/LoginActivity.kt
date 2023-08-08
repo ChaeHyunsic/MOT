dd606d750bf5843e1414d31f0eb1a16ebe059224
ChaeHyunsic <chaehyunsic630@gmail.com>
2023-08-08 14:58:36 +0900

    add activity file about login

---
+++ app/src/main/java/com/example/mot/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mot
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mot.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityLoginBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mBinding!!.findTv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        mBinding!!.loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        mBinding!!.joinTv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
