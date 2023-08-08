84a7b15cad7a126c8a8bd5851d2c066c3fdad803
ChaeHyunsic <chaehyunsic630@gmail.com>
2023-08-08 14:21:04 +0900

    add activity file about splash

--- app/src/main/java/com/example/mot/MainActivity.kt
+++ app/src/main/java/com/example/mot/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/mot/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mot
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mot.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private var mBinding: ActivitySplashBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent= Intent( this,MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+
+        }, 1000)
+    }
+}
