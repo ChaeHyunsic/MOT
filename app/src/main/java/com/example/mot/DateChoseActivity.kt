95f25daace957b349a67a8c78cad915d414137ba
ChaeHyunsic <chaehyunsic630@gmail.com>
2023-08-09 16:32:49 +0900

    add activity file about date chose

--- app/src/main/java/com/example/mot/DateChoseActivity.kt
+++ app/src/main/java/com/example/mot/DateChoseActivity.kt
@@ -1,19 +1,62 @@
 package com.example.mot
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mot.databinding.ActivityDateChoseBinding
+import java.util.*
 
 class DateChoseActivity : AppCompatActivity() {
 
     private var mBinding: ActivityDateChoseBinding? = null
     private val binding get() = mBinding!!
 
+    private var selectedStartDate: Long = 0
+    private var selectedEndDate: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityDateChoseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mBinding!!.calendarView.date = System.currentTimeMillis()
+
+        mBinding!!.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, dayOfMonth)
+
+            if (mBinding!!.startDate.text == "") {
+                selectedStartDate = selectedDate.timeInMillis
+                mBinding!!.startDate.text = year.toString() + "년 " + (month+1).toString() + "월 " + dayOfMonth.toString() + "일"
+            }
+            else {
+                if (selectedDate.timeInMillis > selectedStartDate) {
+                    selectedEndDate = selectedDate.timeInMillis
+                    mBinding!!.endDate.text = year.toString() + "년 " + (month+1).toString() + "월 " + dayOfMonth.toString() + "일"
+                    mBinding!!.choseBtn.visibility = View.VISIBLE
+                }
+                else {
+                    mBinding!!.wrongAnno1.visibility = View.VISIBLE
+                    mBinding!!.wrongAnno2.visibility = View.VISIBLE
+                    mBinding!!.startDate.text = ""
+                    selectedStartDate = 0
+                    mBinding!!.endDate.text = ""
+                    selectedEndDate = 0
+                }
+            }
+        }
+
+        mBinding!!.choseBtn.setOnClickListener {
+            if (mBinding!!.choseBtn.visibility == View.VISIBLE) {
+                val intent = Intent(this, SearchActivity::class.java)
+                intent.putExtra("start_date", mBinding!!.startDate.text)
+                intent.putExtra("end_date", mBinding!!.endDate.text)
+
+                startActivity(intent)
+            }
+        }
 
     }
 }
