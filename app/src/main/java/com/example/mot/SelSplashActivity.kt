93ae0758b937db415c7b976a1336150c54835266
ChaeHyunsic <chaehyunsic630@gmail.com>
2023-08-16 15:29:17 +0900

    add activity file about splash for seller

---
+++ app/src/main/java/com/example/mot/SelSplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mot
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mot.databinding.ActivitySelSplashBinding
+
+class SelSplashActivity : AppCompatActivity() {
+
+    private var mBinding: ActivitySelSplashBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivitySelSplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent= Intent( this,MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+
+        }, 1000)
+    }
+}
